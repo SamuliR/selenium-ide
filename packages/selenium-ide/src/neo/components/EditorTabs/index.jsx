3225a0d81389eeddbb107530ad35dd00b03e52bd
Samuli Reijonen <reijonen.samuli@gmail.com>
2018-11-27 18:04:09 +0200

    feat (dnd) translate dnd-steps to .side format

--- packages/selenium-ide/src/neo/components/EditorTabs/index.jsx
+++ packages/selenium-ide/src/neo/components/EditorTabs/index.jsx
@@ -28,6 +28,123 @@ export default class EditorTabs extends Component {
     this.setState({ dndSteps: steps })
   }
 
+  onPlayClick = (steps) => {
+    console.log('steps:', steps)
+    this.toSide(steps)
+  }
+
+  onPlayStepClick = (step) => {
+    console.log('step:', step)
+  }
+
+  getTarget = (step) => {
+    switch (step.type) {
+      case 'visit':
+        return step.url
+
+      case 'scroll':
+        return step.amoutPercent
+
+      case 'click':
+        return step.selector
+
+      case 'resolution':
+        return ''
+
+      case 'has-text':
+        return 'document.body' //locator
+
+      case 'input':
+        return step.selector
+
+      case 'key-press':
+        return 'document.body' //locator
+
+      case 'sleep':
+        return ''
+
+      case 'navigation':
+        return ''
+    }
+  }
+
+  getValue = (step) => {
+    switch (step.type) {
+      case 'visit':
+        return ''
+
+      case 'scroll':
+        return step.durationSeconds
+
+      case 'click':
+        return ''
+
+      case 'resolution':
+        return step.resolution
+
+      case 'has-text':
+        return step.text
+
+      case 'input':
+        return step.value
+
+      case 'key-press':
+        return step.keys
+
+      case 'sleep':
+        return step.time
+
+      case 'navigation':
+        return step.action
+    }
+  }
+
+  getCommand = (step) => {
+    switch (step.type) {
+      case 'visit':
+        return 'open'
+
+      case 'scroll':
+        return 'scroll'
+
+      case 'click':
+        return 'click'
+
+      case 'resolution':
+        return '' //no such command
+
+      case 'has-text':
+        return 'verify text'
+
+      case 'input':
+        return 'type'
+
+      case 'key-press':
+        return 'send keys'
+
+      case 'sleep':
+        return 'sleep'
+
+      case 'navigation':
+        return '' //no such command
+    }
+  }
+
+  toSide = (steps) => {
+    const sideTest = []
+    steps.forEach(step => {
+      sideTest.concat({
+        id: step.id,
+        comment: '',
+        command: this.getCommand(step),
+        target: this.getTarget(step),
+        targets: [],
+        value: this.getValue(step),
+      })
+    })
+    console.log("sideTest", sideTest)
+  }
+
   render() {
     if (this.state.currentTab === 1) {
       UiState.minimizeConsole()
@@ -65,6 +182,8 @@ export default class EditorTabs extends Component {
             />
           ) : (
             <StepEditor
+              onPlayClick={this.onPlayClick}
+              onPlayStepClick={this.onPlayStepClick}
               saveSteps={this.saveSteps}
               savedSteps={this.state.dndSteps}
             />
