4cc919308eff96c18d2094f39bae5bcbabf65587
Samuli Reijonen <reijonen.samuli@gmail.com>
2018-12-07 17:19:07 +0200

    github auto releases

--- packages/selenium-ide/update.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-webstore upload --source build/build.zip --extension-id $EXTENSION_ID --client-id $CLIENT_ID --client-secret $CLIENT_SECRET --refresh-token '${REFRESH_TOKEN}' --auto-publish
